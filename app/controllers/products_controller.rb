cc13934e79a13a767a7ab4eaa909eef73fcd0ba5
Rodrigo Sommacal <rodrigosommacal@gmail.com>
2020-12-06 22:45:09 +0100

    new product path created

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -15,7 +15,6 @@ class ProductsController < ApplicationController
   # GET /products/new
   def new
     @product = Product.new
-    @assignments = @product.assignments.build
   end
 
   # GET /products/1/edit
