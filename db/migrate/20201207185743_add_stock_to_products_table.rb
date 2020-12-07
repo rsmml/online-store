f9c64dca22e25bdbb9c8ac2114b5a3bd4dca763e
Rodrigo Sommacal <rodrigosommacal@gmail.com>
2020-12-07 19:59:04 +0100

    stock column added to table product

---
+++ db/migrate/20201207185743_add_stock_to_products_table.rb
@@ -0,0 +1,5 @@
+class AddStockToProductsTable < ActiveRecord::Migration[6.0]
+  def change
+    add_column :products, :stock, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_07_153851) do
+ActiveRecord::Schema.define(version: 2020_12_07_185743) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 2020_12_07_153851) do
     t.decimal "price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "stock"
   end
 
   create_table "users", force: :cascade do |t|
