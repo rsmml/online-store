014036d82cebd6be3e6bfb0650f3f3e4b98921c8
Rodrigo Sommacal <rodrigosommacal@gmail.com>
2020-12-08 16:27:48 +0100

    adding some validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,4 +5,9 @@ class Product < ApplicationRecord
 
   accepts_nested_attributes_for :assignments
   has_many :colors, through: :assignments
+
+  validates :title, presence: true
+  validates :price, presence: true
+  validates :stock, presence: true
+  validates :photos, presence: true
 end
