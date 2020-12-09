52e580fb18675e903a746ec9ef189857b2514b03
Rodrigo Sommacal <rodrigosommacal@gmail.com>
2020-12-09 12:40:56 +0100

    adding a destroy all method to orders

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -27,6 +27,11 @@ class OrderItemsController < ApplicationController
     @order_items = current_order.order_items
   end
 
+  def destroy_all
+    @delete_all_orders = current_order.order_items.all
+    @delete_all_orders.destroy_all
+  end
+
   private
 
   def order_params
