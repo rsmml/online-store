56ffc01b32fc05c5f07d02deca77a94db79cf352
Rodrigo Sommacal <rodrigosommacal@gmail.com>
2020-12-09 12:03:54 +0100

    stock counter js error problem solved

--- app/javascript/controllers/stock_controller.js
+++ app/javascript/controllers/stock_controller.js
@@ -11,14 +11,18 @@ export default class extends Controller {
             let object = data.products.filter(obj => {
               return obj.id == this.idTarget.innerText
             });
-            if (object[0].stock === 1) {
-              this.classTarget.classList.value = 'add-to-cart-disable';
-              this.classTarget.hidden = true;
-              this.classTarget.disabled = true;
-              this.countTarget.classList.value = 'out-stock';
-              this.countTarget.innerText = 'You got the last one 😉';
-            } else {
-              this.countTarget.innerText = `Last ${object[0].stock - 1}`;
+            if (object[0].stock <= 10) {
+              if (object[0].stock === 1) {
+                this.classTarget.classList.value = 'add-to-cart-disable';
+                this.classTarget.hidden = true;
+                this.classTarget.disabled = true;
+                this.countTarget.classList.value = 'out-stock';
+                this.countTarget.innerText = 'You got the last one 😉';
+              } else {
+                this.countTarget.innerText = `Last ${object[0].stock - 1}`;
+              }
+            }else{
+              console.log('Hi there hacker!');
             }
           });
   }
