0492865da65c1ddd7c0668aaafd942ed982ce57f
Rodrigo Sommacal <rodrigosommacal@gmail.com>
2020-12-13 11:54:28 +0100

    actions controller created

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,22 +1,46 @@
 class CategoriesController < ApplicationController
+  before_action :find_category, only %i[show edit update destroy]
   def index
+    @categories = Category.all
   end
 
   def show
   end
 
   def new
+    @category = Category.new
   end
 
   def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to category_path(@category)
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    @category.update(category_params)
+    redirect_to category_path(@category)
   end
 
   def destroy
+    @category.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def find_category
+    @category = Category.find(params[:id])
+  end
+
+  def category_params
+    params.require(:category).permite(:name, :description)
   end
 end
--- app/controllers/product_categories_controller.rb
+++ app/controllers/product_categories_controller.rb
@@ -1,22 +1,63 @@
 class ProductCategoriesController < ApplicationController
+  before_action :find_product_category, only: %i[show edit update destroy]
+
   def index
+    @product_categories = ProductCategory.all
   end
 
   def show
   end
 
   def new
+    @product_category = ProductCategory.new
+    @products = Product.all
+    @categories = Category.all
   end
 
   def create
+    find_product
+    find_category
+    @product_category = ProductCategory.new(product_category_params)
+    @product_category.product_id = @product.id
+    @product_category.category_id = @category.id
+
+    if @product_category.save
+      redirect_to products_path
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    @product_category.update(product_category_params)
+
+    redirect_to product_category(@product_category)
   end
 
   def destroy
+    @product_category.destroy
+
+    redirect_to products_path
+  end
+
+  private
+
+  def find_product_category
+    @product_category = ProductCategory.find(params[:id])
+  end
+
+  def find_product
+    @product = Product.find(params[:id])
+  end
+
+  def find_category
+    @category = Category.find(params[:id])
+  end
+
+  def product_category_params
+    params.require(:product_category).permit(:product_id, :category_id)
   end
 end
