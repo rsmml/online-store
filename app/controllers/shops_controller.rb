182750b0928ec859d3c293f90c76224cd7c35ed6
Rodrigo Sommacal <rodrigosommacal@gmail.com>
2020-12-15 18:12:17 +0100

    pundit for shops created with show action

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,11 +1,12 @@
 class ShopsController < ApplicationController
   def index
-    @products = Product.all
+    @products = policy_scope(Product)
 
     @order_item = current_order.order_items.new
   end
 
   def show
     @product = Product.find(params[:id])
+    authorize @product
   end
 end
---
+++ app/policies/shop_policy.rb
@@ -0,0 +1,11 @@
+class ShopPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def show?
+    true
+  end
+end
